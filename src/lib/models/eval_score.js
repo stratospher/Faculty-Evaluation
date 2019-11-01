76d16c5e9c9517188f7e042a89d8b823b87a4c34
Anupama18 <anupamajimmy18@cet.ac.in>
2019-11-01 21:17:10 +0530

    evaluation score

---
+++ src/lib/models/eval_score.js
@@ -0,0 +1,32 @@
+module.exports=function(sequelize,DataTypes){
+	const evalscore=sequelize.define('evalscore',{
+		col_code:{
+			type:DataTypes.STRING(3)
+		},
+		dep_code:{
+			type:DataTypes.STRING(3)
+		},
+		subcode:{
+			type:DataTypes.STRING(7)
+		},
+		subname:{
+			type:DataTypes.STRING(50)
+		},
+		period:{
+			type:DataTypes.STRING(30)
+		},
+		teacher_name:{
+			type:DataTypes.STRING(50)
+		},
+		students:{
+			type:DataTypes.INTEGER()
+		},
+		score:{
+			type:DataTypes.DOUBLE(10,5)
+		},
+
+	});
+	return eval_score;
+
+
+};
